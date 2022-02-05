f3b24806a73008e52e5caa93b44f24842bb61a81
Lee Forbes <leeforbes@Lees-MacBook-Pro.local>
2022-02-05 01:38:24 -0600

    generated hiddenmannypad as a test using formatter

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # utskatespots
+
+Spot Generator Text File Format:
+big title
+background image
+daytime image
+nighttime image
+location
+google maps embed link
+pro
+pro
+pro
+EMPTY LINE
+con
+con
+con
+EMPTY LINE
+image
+image
+image
---
+++ spotgenerator.py
@@ -0,0 +1,94 @@
+import readline
+import sys
+
+print('Attempting to generate webpage for your spot...')
+
+args = sys.argv
+if(len(args) < 3):
+    print('Missing file name argument: ex. spotgenerator.py [input] [output]')
+    sys.exit
+
+print(args[1] + ' -> ' + args[2])
+
+#read in the information from the spot txt file
+spotFile = open(args[1], 'r')
+
+bigTitle = spotFile.readline().rstrip()
+bgImg = spotFile.readline().rstrip()
+dayImg = '<img src="' + spotFile.readline().rstrip() + '" width="100%">'
+nightImg = '<img src="' + spotFile.readline().rstrip() + '" width="100%">'
+location = spotFile.readline().rstrip()
+locationEmbed = spotFile.readline().rstrip().replace('width="600"', 'width="100%"')
+
+pros = []
+proTemp = spotFile.readline().rstrip()
+while(len(proTemp) != 0):
+    pros.append(proTemp)
+    proTemp = spotFile.readline().rstrip()
+
+cons = []
+conTemp = spotFile.readline().rstrip()
+while(len(conTemp) != 0):
+    cons.append(conTemp)
+    conTemp = spotFile.readline().rstrip()
+
+imgs = []
+imgTemp = spotFile.readline().rstrip()
+while(len(imgTemp) != 0):
+    imgs.append(imgTemp)
+    imgTemp = spotFile.readline().rstrip()
+
+spotFile.close()
+
+print('Read ' + bigTitle + ' info!')
+
+#generate HTML for pros, cons, images
+prosHTML = ''
+for p in pros:
+    prosHTML += '<li>' + p + '</li>\n'
+
+consHTML = ''
+for c in cons:
+    consHTML += '<li>' + c + '</li>\n'
+
+imgsHTML = ''
+for i in imgs:
+    imgsHTML += '<div class="image-container">'
+    imgsHTML += '<img src="' + i + '" class="image-inside-div">\n'
+    imgsHTML += '</div>'
+
+#read in the spot page format, copy it to output file
+formatFile = open('spotpageformat.html', 'r')
+outputFile = open(args[2], 'w')
+
+replaceWords = {
+    'REPLACE_BIG_TITLE' : bigTitle,
+    'REPLACE_BACKGROUND_URL_STRING' : bgImg,
+    'REPLACE_DAYTIME_IMAGE' : dayImg,
+    'REPLACE_NIGHTTIME_IMAGE' : nightImg,
+    'REPLACE_GOOGLE_MAPS_EMBED' : locationEmbed,
+    'REPLACE_LOCATION' : location,
+    'REPLACE_LIST_PROS' : prosHTML,
+    'REPLACE_LIST_CONS' : consHTML,
+    'REPLACE_IMAGES' : imgsHTML
+}
+
+formatLine = formatFile.readline().strip()
+while(formatLine != '</body>'): #THIS MIGHT CHANGE!!!
+    if(formatLine.startswith('REPLACE_')):
+        outputFile.write(replaceWords.get(formatLine) + '\n')
+    else:
+        outputFile.write(formatLine + '\n')
+    formatLine = formatFile.readline().strip()
+
+formatFile.close()
+outputFile.close()
+
+print('Generated' + args[2] + '.')
+
+print('Done!')
+
+#open spotpageformat readonly
+#read line by line, stripping whitespace characters -> strip()
+#writeto args[2] file name, replacing when finding REPLACE_...
+#format html?
